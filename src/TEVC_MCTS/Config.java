bd9a975d8f784a5165f93795b9ff75ad91d8f432
ssamot <spyridon.samothrakis@gmail.com>
2014-12-02 22:39:35 +0000

    Corrected discount factor

--- src/TEVC_MCTS/Config.java
+++ src/TEVC_MCTS/Config.java
@@ -24,7 +24,7 @@ public class Config {
     public static int INDIVIDUAL_ITERATIONS = 1;
     public static int ROLLOUT_DEPTH = 10;
     public static double K = Math.sqrt(2);
-    public static double REWARD_DISCOUNT = 0.9; //value of 1.0 equals to no discount
+    public static double REWARD_DISCOUNT = 1.0; //value of 1.0 equals to no discount
     public static double REWARD_STEP_PENALTY = 0; //defines received reward after each step/action; set to < 0.0 to minimize total number of steps
 
     public static boolean OPTIMAL_PLAY_ENABLED = false;
